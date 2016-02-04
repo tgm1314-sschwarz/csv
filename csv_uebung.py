e62b9d65adac05d634a4f6397bf30db3834bc3a8
Stephan Schwarz <sschwarz@student.tgm.ac.at>
2016-02-04 17:26:56 +0100

    tests

---
+++ csv_uebung.py
@@ -0,0 +1,75 @@
+import csv
+
+"""
+Programm welches zwei csv dateien mit unterschiedlichen dialekt einliesst und
+als csv mit einem ; dilekt speichert.
+"""
+__author__ = "Schwarz Stephan"
+__version__ = "1.0"
+
+
+class csv_uebung():
+
+    def __init__(self):
+
+        self.f1 = 'a.csv'
+        self.f2 = 'input2.csv'
+        self.f3 = 'output.csv'
+
+        f1 = self.open_file("input1.csv", "rt")
+        f2 = self.open_file("input2.csv", "rt")
+        f3 = self.open_file("output.csv", "wt")
+
+        d1 = self.get_dialect(f1)
+        d2 = self.get_dialect(f2)
+
+        self.reg_dialect("semicolon", ";")
+
+        r1 = self.read_file(f1, d1)
+        r2 = self.read_file(f2, d2)
+
+        out = self.append_files(r1, r2)
+
+        self.write_file(f3, "semicolon", out)
+
+        self.close_file(f1)
+        self.close_file(f2)
+        self.close_file(f3)
+
+        pass
+
+    def open_file(self, name, type):
+        return open(name, type)
+
+    def get_dialect(self, file):
+        d = csv.Sniffer().sniff(file.read(1024))
+        file.seek(0)
+        return d
+
+    def reg_dialect(self, name, delimiter):
+        csv.register_dialect(name, delimiter=delimiter)
+
+    def read_file(self, file, dialect):
+        return csv.reader(file, dialect)
+
+    def append_files(self, reader1, reader2):
+        out = []
+
+        for row in reader1:
+            out.append(row)
+
+        for row in reader2:
+            out.append(row)
+
+        return out
+
+    def write_file(self, file, dialect, output):
+        writer = csv.writer(file, dialect=dialect)
+        for i in output:
+            writer.writerow(i)
+
+    def close_file(self, file):
+        file.close()
+
+if __name__ == '__main__':
+    csv_uebung()
---
+++ testcsv.py
@@ -0,0 +1,43 @@
+from csv_uebung import *
+from unittest import TestCase
+
+__author__ = 'd4rkor3'
+
+
+class TestCSV(TestCase):
+    def setUp(self):
+        self.csv = csv_uebung()
+        self.f1 = self.csv.open_file("input1.csv", "rt")
+        self.f2 = self.csv.open_file("input2.csv", "rt")
+
+        self.d1 = self.csv.get_dialect(self.f1)
+        self.d2 = self.csv.get_dialect(self.f2)
+
+        self.r1 = self.csv.read_file(self.f1, self.d1)
+        self.r2 = self.csv.read_file(self.f2, self.d2)
+
+        self.out = self.csv.append_files(self.r1, self.r2)
+
+        self.read_test = ['SPOE', 'FPOE', 'OEVP', 'GRUE', 'NEOS', 'WWW', 'ANDAS', 'GFW', 'SLP', 'WIFF', 'M', 'FREIE']
+        self.out_test = [['SPOE', 'FPOE', 'OEVP', 'GRUE', 'NEOS', 'WWW', 'ANDAS', 'GFW', 'SLP', 'WIFF', 'M', 'FREIE'], ['27', '14', '15', '10', '9', '0', '1', '0', '0', '0', '0', '0']]
+
+        pass
+
+    def test_open_file(self):
+        self.assertRaises(FileNotFoundError, self.csv.open_file, "wrong.csv", "rt")
+
+    def test_read_file(self):
+        reader = self.csv.read_file(self.f1, self.d1)
+        for row in reader:
+            assert(row == self.read_test)
+
+    def test_append_files(self):
+        out = self.csv.append_files(self.f1, self.f2)
+        for row in out:
+            assert(row == self.out_test)
+
+    def test_write_file(self):
+        self.assertRaises(TypeError, self.csv.write_file, "wrongType", "semicolon", self.out)
+
+    def test_close_file(self):
+        self.assertRaises(AttributeError, self.csv.close_file, "wrongAttribute")
